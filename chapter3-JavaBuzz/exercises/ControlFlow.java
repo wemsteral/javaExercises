0849a888080ec133a9568291a507226ce4d2acdf
Alasdair <alasdair123@hotmail.co.uk>
2019-07-12 11:46:05 +0100

    5 tasks complete

--- chapter3-JavaBuzz/exercises/ControlFlow.java
+++ chapter3-JavaBuzz/exercises/ControlFlow.java
@@ -1,6 +1,29 @@
 // what imports will you need?
-
+import java.io.*;
+import java.util.Scanner;
 // can you remember how to set up your main function?
+public class ControlFlow {
+public static void main(String[] args) {
+
+        boolean isNotSeventeen = true;
+        while (isNotSeventeen) {
+
+                System.out.println("Please enter a number:");
+
+                Scanner scan = new Scanner(System.in);
+                int i = scan.nextInt();
+                if(i == 17) {
+                        System.out.println("goodbye");
+                        isNotSeventeen = false;
+                }else if(i%2==0) {
+                        System.out.println(i + " is even");
+                }else{
+                        System.out.println(i + " is odd");
+                }
+        }
+
+}
+}
 
 // find a way to read in user input
 
