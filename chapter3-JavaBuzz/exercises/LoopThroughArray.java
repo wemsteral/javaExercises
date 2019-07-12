db48e0d86428fc0f66343979532185346a920c55
Alasdair <alasdair123@hotmail.co.uk>
2019-07-12 12:00:03 +0100

    refactor format and complete LoopThroughArray chapter3

--- chapter3-JavaBuzz/exercises/LoopThroughArray.java
+++ chapter3-JavaBuzz/exercises/LoopThroughArray.java
@@ -1,3 +1,14 @@
 // can you remember how to set up your main function?
 
 // loop through an integer array and print out each element
+
+public class LoopThroughArray {
+public static void main(String[] args) {
+
+        String[] items = {"one","two","three","four","five"};
+        for(String item : items) {
+                System.out.println (item);
+        }
+
+}
+}
